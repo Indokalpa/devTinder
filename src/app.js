7609aee94216617cf8f3f1af2a332c0d6a6e4954
indokalpa <indokalpasaikia1@gmail.com>
2025-10-30 16:16:58 +0530

    /user & /feed api

--- src/app.js
+++ src/app.js
@@ -18,6 +18,32 @@ app.post("/signup", async (req, res) => {
     }catch(err){
         res.status(400).send("error saving the message" + err.message);
     }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await UserModel.findOne({emailId: userEmail});
+        if(!user){
+            res.status(400).send("error fetching the user" + err.message);
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+        res.status(400).send("error fetching the user" + err.message);
+    }
+})
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+    try{
+        const users = await UserModel.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("error fetching the feed" + err.message);
+    }
 })
 
 connectDB().then(() => {
